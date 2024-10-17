63ba64993a3da3733923aa4fff8b3d28549ef5b3
avaldezp22 <avaldez@igp.gob.pe>
2024-10-17 08:53:38 -0500

    update

--- winds_amisr_5ch.py
+++ winds_amisr_5ch.py
@@ -25,23 +25,7 @@ def main():
     tiempo=['0','24']
 
     localtime='1' #para ajustar el horario en las gráficas '0' para dejar en utc
-
-    dty = datetime.date.today()                   #ONLINE
-    str1 = dty + datetime.timedelta(days=1)
-    str2 = dty - datetime.timedelta(days=1)
-    #dty = dty - datetime.timedelta(days=1)  #diferencia restando 1 , probando data del 21
-    today = dty.strftime("%Y/%m/%d")
-    tomorrow = str1.strftime("%Y/%m/%d")
-    yesterday = str2.strftime("%Y/%m/%d")
-    startDate=today
-    #startDate="2022/11/12"
-    # endDate=tomorrow # se esta comentando para este caso particular y se colocara el mismo dia
-    endDate=today
-    ##.......................................................................................
-    l = startDate.split('/')                        #adding day of the year to outPath
-    datelist = datetime.date(int(l[0]),int(l[1]),int(l[2]))
-    DOY = datelist.timetuple().tm_yday
-    outPath= outPath+"/online/"+l[0]+str(DOY).zfill(3)
+    outPath= outPath+"/online/"+"2024235"
     if os.path.exists(outPath):
         print("outPath", outPath)
     else :
@@ -103,7 +87,7 @@ def main():
     opObj03.addParameter(name='maxHei', value='350', format='int')
     opObj03.addParameter(name='minFreq', value='-40000', format='float')
     opObj03.addParameter(name='maxFreq', value='40000', format='float')
-
+    '''
     #SpectraPlot
     opObj12 = spc_proc.addOperation(name='SpectraPlot', optype='external')
     opObj12.addParameter(name='wintitle', value='SPC ISR-AMISR', format='str')
@@ -123,8 +107,8 @@ def main():
     opObj12.addParameter(name='server', value=realtime_server)
     opObj12.addParameter(name='sender_period', value='60')
     opObj12.addParameter(name='tag', value='AMISR')
-
-
+    '''
+    
 
     ##.......................................................................................
     #--------------------    Parameters Processing Unit up  ------------
@@ -132,7 +116,7 @@ def main():
     param_proc = controllerObj.addProcUnit(datatype='ParametersProc', inputId=spc_proc.getId())
     opObj41 = param_proc.addOperation(name='SpectralMoments', optype='other')
 
-
+    '''
     opObj42 = param_proc.addOperation(name='SpectralMomentsPlot', optype='other')
     opObj42.addParameter(name='id', value='003', format='int')
     opObj42.addParameter(name='ymin', value='0', format='int')
@@ -140,6 +124,7 @@ def main():
     opObj42.addParameter(name='zmin', value=db_range[0], format='int')
     opObj42.addParameter(name='zmax', value=db_range[1], format='int')
     opObj42.addParameter(name='save', value=outPath, format='str')
+    '''
     '''
     opObj42 = param_proc.addOperation(name='DopplerPlot', optype='other')
     opObj42.addParameter(name='id', value='005', format='int')
@@ -151,14 +136,17 @@ def main():
     opObj42.addParameter(name='SNRmax', value='6', format='int')
     opObj42.addParameter(name='save', value=outPath, format='str')
     opObj42.addParameter(name='showSNR', value='1', format='bool')
-    ''' 
+    '''
     ##.......................................................................................
     #--------------------------NEW--------------
+    '''
     opObj52 = param_proc.addOperation(name='HDFWriter', optype='other')
     opObj52.addParameter(name='path', value=procpath)
     opObj52.addParameter(name='blocksPerFile', value='100', format='int')
+    #opObj52.addParameter(name='metadataList', value='type,inputUnit,heightList,paramInterval', format='list')
     opObj52.addParameter(name='metadataList', value='type,inputUnit,heightList,paramInterval,timeZone', format='list')
     opObj52.addParameter(name='dataList', value='moments,data_snr,noise,utctime,utctimeInit', format='list')
+    '''
     ##opObj52.addParameter(name='dataList', value='data_param,data_SNR,noise,utctime,utctimeInit', format='list')
     ##opObj52.addParameter(name='mode', value='1', format='int')
     
@@ -169,7 +157,7 @@ def main():
     opObj53.addParameter(name='correctAzimuth', value='0', format='float')
     #opObj23.addParameter(name='correctAzimuth', value='52.5414', format='float')
     opObj53.addParameter(name='correctFactor', value='-1', format='float')
-
+    '''
 
     titles=('Zonal Wind,Meriodinal Wind,Vertical Wind')
     
@@ -180,11 +168,14 @@ def main():
     opObj54.addParameter(name='save', value=outPath)# figpath
     opObj54.addParameter(name='titles', value=titles)
     opObj54.addParameter(name='zfactors', value='1,1,1')
-    opObj54.addParameter(name='zlimits', value='(-100,100),(-100,100),(-60,60)')
+    opObj54.addParameter(name='zlimits', value='(-25,25),(-25,25),(-20,20)')# se esta consideranddo cuarta y tercera parte
     opObj54.addParameter(name='cb_labels', value='m/s,m/s,cm/s')
     opObj54.addParameter(name='throttle', value='1')
-    opObj54.addParameter(name='xmin', value=0)
-    opObj54.addParameter(name='xmax', value=24)
+    opObj54.addParameter(name='xmin', value=1)
+    opObj54.addParameter(name='xmax', value=18)
+    opObj54.addParameter(name='ymin', value='0', format='int')
+    opObj54.addParameter(name='ymax', value='10', format='int')
+    '''
     
     opObj55 = param_proc.addOperation(name='HDFWriter', optype='other')
     opObj55.addParameter(name='path', value=procpath2)
@@ -192,7 +183,7 @@ def main():
     opObj55.addParameter(name='metadataList',value='type,inputUnit,outputInterval',format='list')
     opObj55.addParameter(name='dataList',value='data_output,utctime,heightList',format='list')    
     #opObj55.addParameter(name='mode', value='1', format='int')
-
+    
     controllerObj.start()
     controllerObj.join()
 
